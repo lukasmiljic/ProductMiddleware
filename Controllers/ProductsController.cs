fc570eafcedabf0f60de45b4d62b84cb2646a9d5
lukasmiljic <smiljicluka13@gmail.com>
2024-06-23 21:41:11 +0200

    add swagger documentation

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -15,12 +15,19 @@ namespace ProductMiddleware.Controllers
             _productService = productService;
         }
 
+        /// <summary>
+        /// Retrieves all the products
+        /// </summary>
         [HttpGet]
         public async Task<IEnumerable<Product>> GetProducts()
         {
             return await _productService.GetProductsAsync();
         }
 
+        /// <summary>
+        /// Retrieves product by id
+        /// </summary>
+        /// <param name="id">Product id</param>
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
@@ -34,12 +41,22 @@ namespace ProductMiddleware.Controllers
             return product;
         }
 
+        /// <summary>
+        /// Searches products by query
+        /// </summary>
+        /// <param name="query">Search query</param>
         [HttpGet("search")]
         public async Task<IEnumerable<Product>> SearchProducts([FromQuery] string query)
         {
             return await _productService.SearchProductsAsync(query);
         }
 
+        /// <summary>
+        /// Filters products by category, min price and max price
+        /// </summary>
+        /// <param name="category">Category</param>
+        /// <param name="minPrice">Minimum price</param>
+        /// <param name="maxPrice">Maximum price</param>
         [HttpGet("filter")]
         public async Task<IEnumerable<Product>> FilterProducts([FromQuery] string category, [FromQuery] decimal? minPrice, [FromQuery] decimal? maxPrice)
         {
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using ProductMiddleware.Services;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,7 +7,18 @@ builder.Services.AddControllers();
 builder.Services.AddHttpClient<RestProductService>();
 builder.Services.AddScoped<IProductService, RestProductService>();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Product middleware API", 
+        Description = "Middleware REST API that retrieves and displays products with filtering capabilities. Created for King ICT internship", 
+        Version = "v1" 
+    });
+});
 
 var app = builder.Build();
 
@@ -14,7 +26,11 @@ if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product middleware API");
+        c.DocumentTitle = "Product middleware API";
+    });
 }
 
 app.UseHttpsRedirection();
